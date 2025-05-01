2e617374d5fda75cff7c138a82f9e7fbe57fbd47
Julian Romero <Juliban27@gmail.com>
2025-05-01 15:58:16 -0500

    mejora login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -34,27 +34,35 @@ const Login = () => {
 
     return (
         <div className=' font-abinet-regular'>
-            <div className='bg-[#E0EDFF]  h-[50vw] rounded-b-4xl absolute inset-0'></div>
-            <div className='text-black relative z-10'>
-                <form onSubmit={handleLogin} className='flex flex-col items-center justify-center h-screen'>
+            <div className='bg-[#E0EDFF]  h-[50vw] rounded-b-4xl absolute inset-0 flex items-center justify-center'>
+                <div>
+                    <h3 className='text-2xl'>Unisabana</h3>
+                    <h1 className='text-7xl text-[#001C63]'>Dining Through</h1>
+                    <h4>Log-In!</h4>
+                </div>
+            </div>
+
+            <div className='text-black relative z-10 flex flex-col items-center h-screen justify-center pt-6'>
+                <form onSubmit={handleLogin} className='flex flex-col items-center justify-center'>
                     <div>
-                        <label>Email</label>
                         <input type="email"
                             value={email}
                             onChange={(e)=>setEmail(e.target.value)} required
                             className='border-2 border-gray-300 rounded-md p-2 mb-4'
+                            placeholder='Correo electronico'
                         />
                     </div>
                     <div>
-                        <label>Password</label>
                         <input type="password"
                             value={password}
                             onChange={(e)=>setPassword(e.target.value)} required
+                            placeholder='Contraseña'
                         />
                     </div>
+                    <p className='text-purple-900'>No tienes cuenta?</p>
                     {error && <p className='text-red-500'>{error}</p>}
                     <Button
-                        text={"Presionar"}
+                        text={"Acceder"}
                         onClick={handleLogin}
                         className={" "}
                         disabled={loading}
